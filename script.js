c060878a985c8d1267466fc281960595b51a2a86
Rahmatxonov <rahmatxonov1001@gmail.com>
2024-02-18 14:56:36 +0500

    start vs pouse

---
+++ script.js
@@ -0,0 +1,21 @@
+const box = document.querySelector(".box");
+const start = document.querySelector(".start");
+const pouse = document.querySelector(".pouse");
+const num = document.querySelector(".num");
+
+let number = 0;
+let timeInterval;
+start.addEventListener("click", () => {
+  timeInterval = setInterval(() => {
+    number++;
+    num.classList.add("num");
+    num.textContent = `${number}`;
+  }, 1000);
+});
+
+pouse.addEventListener("click", () => {
+  setTimeout(() => {
+    clearTimeout(timeInterval);
+    num.classList.remove("num");
+  });
+});
