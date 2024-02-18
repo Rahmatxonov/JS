4990dbb2b03b2d53bfcefb8a8ff9c3e5d48e9bda
Rahmatxonov <rahmatxonov1001@gmail.com>
2024-02-18 17:37:28 +0500

    date time

--- script.js
+++ script.js
@@ -1,21 +1,67 @@
-const box = document.querySelector(".box");
-const start = document.querySelector(".start");
-const pouse = document.querySelector(".pouse");
-const num = document.querySelector(".num");
+const card = document.querySelector(".card");
+const time = document.querySelector(".time");
 
-let number = 0;
-let timeInterval;
-start.addEventListener("click", () => {
-  timeInterval = setInterval(() => {
-    number++;
-    num.classList.add("num");
-    num.textContent = `${number}`;
-  }, 2000);
-});
+const now = new Date();
 
-pouse.addEventListener("click", () => {
-  setTimeout(() => {
-    clearTimeout(timeInterval);
-    num.classList.remove("num");
-  });
-});
+let hours = document.createElement("h2");
+let minutes = document.createElement("h2");
+let seconds = document.createElement("h2");
+let day = document.createElement("h2");
+let month = document.createElement("h2");
+let year = document.createElement("h2");
+
+day.classList.add("day");
+month.classList.add("month");
+year.classList.add("year");
+hours.classList.add("hour");
+minutes.classList.add("minutes");
+seconds.classList.add("seconds");
+card.classList.add("card");
+
+const getDay = now.getDate() < 10 ? "0" + now.getDate() : now.getDate();
+const getMonth = now.getMonth() < 10 ? "0" + now.getMonth() : now.getMonth();
+const getYear =
+  now.getFullYear() < 10 ? "0" + now.getFullYear() : now.getFullYear();
+const getHours = now.getHours() < 10 ? "0" + now.getHours() : now.getHours();
+const getMinutes =
+  now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
+const getSeconds =
+  now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds();
+
+day.textContent = getDay + ".";
+month.textContent = getMonth + ".";
+year.textContent = getYear;
+hours.textContent = getHours + ":";
+minutes.textContent = getMinutes + ":";
+seconds.textContent = getSeconds;
+
+time.appendChild(hours);
+time.appendChild(minutes);
+time.appendChild(seconds);
+card.appendChild(day);
+card.appendChild(month);
+card.appendChild(year);
+
+setInterval(updateTime, 1000);
+
+function updateTime() {
+  const now = new Date();
+
+  const getDay = now.getDate() < 10 ? "0" + now.getDate() : now.getDate();
+  const getMonth =
+    now.getMonth() + 1 < 10 ? "0" + (now.getMonth() + 1) : now.getMonth() + 1;
+  const getYear =
+    now.getFullYear() < 10 ? "0" + now.getFullYear() : now.getFullYear();
+  const getHours = now.getHours() < 10 ? "0" + now.getHours() : now.getHours();
+  const getMinutes =
+    now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
+  const getSeconds =
+    now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds();
+
+  day.textContent = getDay + ".";
+  month.textContent = getMonth + ".";
+  year.textContent = getYear;
+  hours.textContent = getHours + ":";
+  minutes.textContent = getMinutes + ":";
+  seconds.textContent = getSeconds;
+}
